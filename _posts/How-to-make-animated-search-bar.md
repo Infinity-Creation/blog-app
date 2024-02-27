8d0b2db55eb7bd7967977aca1affcaefae436d2d
Infinity-Creation <leon.deliusf@gmail.com>
2024-02-27 19:06:06 +0100

    da;sdkla;s

--- _posts/How-to-make-animated-search-bar.md
+++ _posts/How-to-make-animated-search-bar.md
@@ -61,10 +61,12 @@ Make sure to paste it after the `<input>` as shown below.
 ```xml
  <div>
         <input type="text" placeholder="Search Anything...">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+         viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
             class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round"
-                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 
+                5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
         </svg>
     </div>
 ```
@@ -149,10 +151,12 @@ HTML:
 <body>
     <div>
         <input type="text" placeholder="Search Anything...">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" 
+        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
             class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round"
-                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                d="m21 21-5.197-5.197m0 0A7.5 
+                7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
         </svg>
     </div>
 </body>
--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -3,24 +3,18 @@ import { EXAMPLE_PATH } from "@/lib/constants";
 
 export function Footer() {
   return (
-    <footer className="bg-neutral-50 border-t border-neutral-200">
+    <footer className="bg-neutral-950 border-t border-white text-white">
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
+            Alteca.xyz
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              href="https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts" target={"_blank"}
+              className="mx-3 bg-blue-600 rounded-xl hover:bg-white hover:size() hover:text-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
-              Read Documentation
-            </a>
-            <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-              className="mx-3 font-bold hover:underline"
-            >
-              View on GitHub
+              Start Reading
             </a>
           </div>
         </div>
