856cc5eb5fcfe486a2803f11012a61bea0c0e197
Infinity-Creation <leon.deliusf@gmail.com>
2024-02-28 09:33:36 +0100

    deleted size temp

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -12,7 +12,7 @@ export function Footer() {
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
               href="https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts" target={"_blank"}
-              className="mx-3 bg-blue-600 rounded-xl hover:bg-white hover:-translate-y-10 hover:size() hover:text-black text-white font-bold py-3 px-12 lg:px-8 duration-1000 transition mb-6 lg:mb-0"
+              className="mx-3 bg-blue-600 rounded-xl hover:bg-white hover:-translate-y-6 hover:text-black text-white font-bold py-3 px-12 lg:px-8 duration-500 transition mb-6 lg:mb-0"
             >
               Start Reading
             </a>
