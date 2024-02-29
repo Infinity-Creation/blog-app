fa5749c19f15ffea54c3bc1d3caaedf833819bf8
Infinity-Creation <leon.deliusf@gmail.com>
2024-02-29 08:57:54 +0100

    cta temp

--- src/app/_components/cta.tsx
+++ src/app/_components/cta.tsx
@@ -2,9 +2,15 @@ import Container from "./container";
 export function CallToAction() {
     return (
        <Container>
-    <h1>Alteca</h1>
-    <h2><div className="bg-gradient-to-r from-blue-600 to-violet-700">Explore</div> Programming tutorials, Reviews,</h2>
-    <h2><div className="bg-gradient-to-r from-blue-600 to-violet-700">Learn</div> TailwindCSS, JavaScript, React and Typescript</h2>
+
+    <h1 className="text-8xl font-bold">Alteca</h1>
+    <h2 className="font-bold text-5xl mb-[10000px]">Explore Programming Tutorials, Reviews, and How-to-Guides</h2>
+    <a
+              href="https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts" target={"_blank"}
+              className="mx-3 bg-black border-[1px] border-solid border-[white] rounded-2xl hover:bg-white hover:-translate-y-4 hover:text-black text-white font-bold py-3 px-12 lg:px-8 duration-500 transition mb-6 lg:mb-0 " 
+            >
+              Start Reading
+            </a>
     </Container>
 
 );
