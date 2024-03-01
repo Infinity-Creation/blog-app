c311ba2a37bd70d04f61511894df0c50e15043bb
Lidia D <lidia.khmylko@gmail.com>
2024-03-01 21:58:43 +0100

    add background image

--- src/app/_components/cta.tsx
+++ src/app/_components/cta.tsx
@@ -1,20 +1,30 @@
-import Container from "./container";
-export function CallToAction() {
-    return (
-       <Container>
-       
-        <section className="bg-[url(/public/assets/home/3d-lines.jpg)]
-">
-    <h1 className="text-8xl mt-[100px] font-bold">Alteca</h1>
-    <h2 className="font-bold text-5xl mb-[50px]">Explore Programming Tutorials, Reviews, and How-to-Guides</h2>
-    <a
-              href="/blog"
-              className="mx-3 bg-black border-[1px] border-solid border-[white] rounded-2xl hover:bg-white hover:-translate-y-4 hover:text-black text-white font-bold py-3 px-12 lg:px-8 duration-500 transition mb-6 lg:mb-0 " 
-            >
-              Start Reading
-            </a>
-            </section>
-    </Container>
+import Image from "next/image";
 
-);
-    }
+export function CallToAction() {
+  return (
+      <section>
+        <div className="relative h-screen w-screen overflow-hidden">
+          <Image
+            alt="3d Form"
+            src="/assets/home/3d-lines.jpg"
+            quality={100}
+            fill={true}
+            objectFit="cover"
+            sizes="100vw"
+          />
+        </div>
+        <div className="absolute -translate-x-2/4 -translate-y-2/4 left-2/4 top-2/4 z-1 bg-transparent">
+          <h1 className="text-8xl mt-[100px] font-bold bg-transparent">Alteca</h1>
+          <h2 className="font-bold text-5xl mb-[50px] bg-transparent">
+            Explore Programming Tutorials, Reviews, and How-to-Guides
+          </h2>
+          <a
+            href="/blog"
+            className="mx-3 border-[1px] border-solid border-[white] rounded-2xl hover:bg-white hover:-translate-y-4 hover:text-black text-white font-bold py-3 px-12 lg:px-8 duration-500 transition mb-6 lg:mb-0 "
+          >
+            Start Reading
+          </a>
+        </div>
+      </section>
+  );
+}
