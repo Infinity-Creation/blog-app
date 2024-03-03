257f8a9c403f7c54fed0a518243bfa9b7005a9ab
Infinity-Creation <leon.deliusf@gmail.com>
2024-03-03 13:57:43 +0100

    update techstack

--- src/app/_components/tech-stack.tsx
+++ src/app/_components/tech-stack.tsx
@@ -5,37 +5,29 @@ import Image from "next/image";
 export function TechStack() {
   return (
     <section>
-      <h2>My Tech Stack</h2>
+      <h2 className="font-bold text-6xl text-center">My Tech Stack</h2>
       <div className="flex justify-center gap-[50px]">
-        <div>
-          <p>
+        <div className=" mr-[100px] ml-[100px] p-[50px] border-[1px] border-[white] border-solid rounded-2xl">
+          <p className="text-center">
             I am very fluent in HTML 5 and CSS 3 as well as in JavaScript. I
             also am starting to master Node.js and other more advanced JS and TS
             frameworks like React, I hope to one day become a master of web
             development technologies, but right now that is far from reality.
           </p>
-          <p>
+          <p className="mt-[20px] text-center">
             {" "}
             This blog is made using TypeScript React Node.js Next.js and
             TailwindCSS.
           </p>
         </div>
-        <div className="flex grid-cols-3 grid-rows-4">
-          <div>
-            <Image
-              priority={true}
-              src="/assets/devicons/javascript-original.svg"
-              height={96}
-              width={96}
-              alt="JavaScript"
-            />
-          </div>
+        <div className="flex grid grid-cols-4 grid-rows-3 gap-[30px]">
+          
           <div>
           <Image
               priority={true}
               src="/assets/devicons/html5-original.svg"
-              height={96}
-              width={96}
+              height={200}
+              width={200}
               alt="HTML5"
             />
           </div>
@@ -43,16 +35,43 @@ export function TechStack() {
           <Image
               priority={true}
               src="/assets/devicons/css3-original.svg"
-              height={96}
+              height={200}
               width={96}
               alt="CSS3"
+            />
+          </div>
+          <div>
+            <Image
+              priority={true}
+              src="/assets/devicons/javascript-original.svg"
+              height={1}
+              width={200}
+              alt="JavaScript"
+            />
+          </div>
+          <div>
+          <Image
+              priority={true}
+              src="/assets/devicons/typescript-original.svg"
+              height={200}
+              width={96}
+              alt="TypeScript"
+            />
+          </div>
+          <div>
+          <Image
+              priority={true}
+              src="/assets/devicons/figma-original.svg"
+              height={200}
+              width={96}
+              alt="Figma"
             />
           </div>
           <div>
           <Image
               priority={true}
               src="/assets/devicons/chrome-original.svg"
-              height={96}
+              height={200}
               width={96}
               alt="Chrome"
             />
@@ -61,7 +80,7 @@ export function TechStack() {
           <Image
               priority={true}
               src="/assets/devicons/opera-original.svg"
-              height={96}
+              height={200}
               width={96}
               alt="Opera"
             />
@@ -70,7 +89,7 @@ export function TechStack() {
           <Image
               priority={true}
               src="/assets/devicons/firefox-original.svg"
-              height={96}
+              height={200}
               width={96}
               alt="Firefox"
             />
@@ -78,35 +97,8 @@ export function TechStack() {
           <div>
           <Image
               priority={true}
-              src="/assets/devicons/figma-original.svg"
-              height={96}
-              width={96}
-              alt="Figma"
-            />
-          </div>
-          <div>
-          <Image
-              priority={true}
-              src="/assets/devicons/git-original.svg"
-              height={96}
-              width={96}
-              alt="Git"
-            />
-          </div>
-          <div>
-          <Image
-              priority={true}
-              src="/assets/devicons/nodejs-original.svg"
-              height={96}
-              width={96}
-              alt="Nodejs"
-            />
-          </div>
-          <div>
-          <Image
-              priority={true}
               src="/assets/devicons/react-original.svg"
-              height={96}
+              height={200}
               width={96}
               alt="React"
             />
@@ -114,17 +106,29 @@ export function TechStack() {
           <div>
           <Image
               priority={true}
-              src="/assets/devicons/typescript-original.svg"
-              height={96}
+              src="/assets/devicons/nodejs-original.svg"
+              height={200}
               width={96}
-              alt="TypeScript"
+              alt="Nodejs"
             />
           </div>
           <div>
           <Image
               priority={true}
+              src="/assets/devicons/git-original.svg"
+              height={200}
+              width={96}
+              alt="Git"
+            />
+          </div>
+          
+          
+          
+          <div>
+          <Image
+              priority={true}
               src="/assets/devicons/vscode-original.svg"
-              height={96}
+              height={200}
               width={96}
               alt="VSCode"
             />
