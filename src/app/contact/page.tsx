381e145bf12fbe85e7a5ddef891423ae560bcc13
Infinity-Creation <leon.deliusf@gmail.com>
2024-03-04 17:21:44 +0100

    update contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,15 +7,9 @@ export default function Contact() {
     <section className="bg-neutral-950 text-white">
       <Container>
         <NavBar></NavBar>
-        <h1 className="text-8xl mt-[100px] font-bold">Contact</h1>
-        <h2 className="text-6xl mt-[50px] font-bold" >Social</h2>
-        <ul>
-          <li><a href="https://dev.to/alteca" className="text-2xl text-blue-600">DEV Community</a></li>
-          <li><a href="https://scratch.mit.edu/users/Infinity-creations/" className="text-2xl text-blue-600">Scratch</a></li>
-          <li><a href="https://x.com/@Alteca_xyz" className="text-2xl text-blue-600 ">X/Twitter</a></li>
-          <li><a href="https://alteca.hashnode.dev" className="text-2xl text-blue-600">Hashnode</a></li>
-          </ul>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
+        <h1 className="text-8xl mt-[100px] font-bold text-center">Contact</h1>
+        
+          <div className="flex  justify-center">
             <a 
               href="mailto:#"
               className=" mx-3 bg-black border-[1px] border-solid border-[white] rounded-2xl hover:bg-white hover:-translate-y-2 hover:text-black text-white font-bold py-3 px-12 lg:px-8 duration-500 transition mb-6 lg:mb-0"
