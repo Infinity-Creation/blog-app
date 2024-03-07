58498c0a67b10e15c28c0c5cae736d3c6ace6e91
Infinity-Creation <leon.deliusf@gmail.com>
2024-03-07 17:20:08 +0100

    update blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,15 +1,18 @@
 import Container from "@/app/_components/container";
 import { HeroPost } from "../_components/hero-post";
 import { MoreStories } from "../_components/more-stories";
+import { getAllPosts } from "../../lib/api";
 import { NavBar } from "@/app/_components/navbar";
 
 export default function Blog() {
+  const allPosts = getAllPosts();
   return (
     
     <Container>
     <section className="bg-neutral-950 text-white">
     <NavBar></NavBar>
     <h1 className="text-8xl mt-[100px] font-bold">Alteca Blog</h1>
+    <MoreStories posts={allPosts}></MoreStories>
     </section>
   </Container>
     
