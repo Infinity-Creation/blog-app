f335c73ca3d7da61820a5885c8fa8d5e0873c726
Infinity-Creation <leon.deliusf@gmail.com>
2024-03-19 17:43:40 +0100

    I do not care

--- src/app/_components/cta.tsx
+++ src/app/_components/cta.tsx
@@ -16,9 +16,9 @@ export function CallToAction() {
             </div>
             <div className=" md:ml-[-100px] absolute -translate-x-2/4 -translate-y-2/4 left-2/4 top-2/4 z-1 bg-transparent">
                 
-                <h1 className="text-7xl mt-[100px] font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-blue-600">The Fullstack Resource you need</h1>
-                <h2 className="font-bold text-5xl mb-[50px] ">
-                    Explore Programming Tutorials, Reviews, and How-to-Guides
+                <h1 className="text-7xl mt-[100px] font-bold flex"> <div className="text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-blue-600 mr-[10px]">Unleash </div> Your Code</h1>
+                <h2 className="font-600 text-3xl mb-[50px] ">
+                Dive into the world of fullstack web development and build amazing projects with me.
                 </h2>
                 <div className=" bg-transparent flex align-center">
                     <a
